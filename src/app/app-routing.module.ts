19596f6ef6c6ea57ff0856029c0aa995f1c2e8b0
lanjewarakash <lanjewarakash72@gmail.com>
2023-02-16 23:17:02 +0530

    [Akash] Added signIn UI page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 const routes: Routes = [
   { path: 'registration', component:RegistrationComponent },
+  {path: 'signin', component:SigninComponent}
 ];
 
 @NgModule({
