61a1d712ddfe14c505bb88febde2ad50b4f594e6
lanjewarakash <lanjewarakash72@gmail.com>
2023-02-17 01:06:13 +0530

    [Akash] Added Validation and Data Binding page

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit {
+  forgotpassword!: FormGroup;
+    submitted = false;
 
-}
+    constructor(private formBuilder: FormBuilder) {}
+  
+    ngOnInit() {
+      this.forgotpassword = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+      });
+    }
+
+    onsubmit() {
+      if (this.forgotpassword.valid) {
+        console.log('onsubmit function called', this.forgotpassword.value);
+        let data = {
+          email: this.forgotpassword.value.email,
+        };
+      }
+    }
+  
+  }
+
+
