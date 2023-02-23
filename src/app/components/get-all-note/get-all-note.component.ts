478a119d8ca35099262b68da93208719bce7490e
lanjewarakash <lanjewarakash72@gmail.com>
2023-02-24 02:08:02 +0530

    [Akash] Added API intregation

--- src/app/components/get-all-note/get-all-note.component.ts
+++ src/app/components/get-all-note/get-all-note.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoteService } from 'src/app/service/noteService/note.service';
 
 @Component({
   selector: 'app-get-all-note',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class GetAllNoteComponent {
 
-}
+  constructor(private noteService:NoteService){
+
+  }
+  ngOnInit(): void {
+
+    this.getAllNotes()
+    
+  }
+  getAllNotes(){
+    this.noteService.getAllNote().subscribe((response : any) => {
+        console.log(response.data.success),
+        console.log(response.data.data);
+
+    })
+
+  }
+
+  }
+
